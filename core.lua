e15abec4b24b17a6deecaab2f7409607fbbba123
gashole <gashole@users.noreply.github.com>
2017-02-15 14:21:48 -0800

    Add support for modui-FocusFrame

--- core.lua
+++ core.lua
@@ -1,7 +1,8 @@
 
 
-    local TEXTURE   = [[Interface\AddOns\modui\statusbar\texture\name2.tga]]
-    local orig      = {}
+    local TEXTURE       = [[Interface\AddOns\modui\statusbar\texture\name2.tga]]
+    local FRAME_TEXTURE = [[Interface\AddOns\modui-TallHealthBar\Textures\UI-TargetingFrame]]
+    local orig          = {}
 
     orig.TargetFrame_CheckClassification    = TargetFrame_CheckClassification
     orig.HealthBar_OnValueChanged           = HealthBar_OnValueChanged
@@ -13,7 +14,7 @@
     PlayerFrameHealthBar:SetPoint('TOPLEFT', 106, -22)
     PlayerFrameHealthBar:SetStatusBarTexture(TEXTURE)
 
-    PlayerFrameTexture:SetTexture[[Interface\AddOns\modui-TallHealthBar\Textures\UI-TargetingFrame]]
+    PlayerFrameTexture:SetTexture(FRAME_TEXTURE)
     PlayerStatusTexture:SetTexture[[Interface\AddOns\modui-TallHealthBar\Textures\UI-Player-Status]]
 
     TargetFrameNameBackground:Hide()
@@ -24,7 +25,7 @@
 
     function TargetFrame_CheckClassification()
         orig.TargetFrame_CheckClassification()
-        TargetFrameTexture:SetTexture[[Interface\AddOns\modui-TallHealthBar\Textures\UI-TargetingFrame]]
+        TargetFrameTexture:SetTexture(FRAME_TEXTURE)
     end
 
     function HealthBar_OnValueChanged(v, smooth)
@@ -33,6 +34,33 @@
             local _, class = UnitClass(this.unit)
             local colour = RAID_CLASS_COLORS[class]
             this:SetStatusBarColor(colour.r, colour.g, colour.b)
+        end
+    end
+
+    if IsAddOnLoaded'modui-FocusFrame' then
+        local _, class = UnitClass'player'
+
+        orig.FocusFrame_CheckClassification = FocusFrame_CheckClassification
+        orig.FocusFrame_HealthUpdate        = FocusFrame_HealthUpdate
+
+        FocusFrameNameBackground:Hide()
+        FocusFrameHealthBar:SetHeight(29)
+        FocusFrameHealthBar:SetPoint('TOPLEFT', 6, -22)
+        FocusFrameHealthBar:SetStatusBarTexture(TEXTURE)
+
+        function FocusFrame_CheckClassification(unit)
+            orig.FocusFrame_CheckClassification(unit)
+            FocusFrameTexture:SetTexture(FRAME_TEXTURE)
+            if UnitIsPlayer(unit) then _, class = UnitClass(unit) end
+        end
+
+        function FocusFrame_HealthUpdate(unit)
+            orig.FocusFrame_HealthUpdate(unit)
+            local data = FocusFrame_GetFocusData(CURR_FOCUS_TARGET)
+            if data.npc == '1' then
+                local colour = RAID_CLASS_COLORS[class]
+                FocusFrameHealthBar:SetStatusBarColor(colour.r, colour.g, colour.b)
+            end
         end
     end
 
